fcd93a3008d35a100c8c221ce5025928462688cc
ujjwalbharadwaj <ujjwalbhardwaj5809@gmail.com>
2024-05-27 15:40:12 +0530

    app  updated

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,20 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   });
 }
 
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 // import routes
 const user = require("./controller/user");
 const shop = require("./controller/shop");
--- backend/server.js
+++ backend/server.js
@@ -14,19 +14,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
     path: "config/.env",
   });
 }
-const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
 // connect db
 connectDatabase();
 
