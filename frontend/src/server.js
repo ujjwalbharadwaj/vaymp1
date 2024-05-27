afb09469922d9c853b9ff59f32ecd508e123c553
ujjwalbharadwaj <ujjwalbhardwaj5809@gmail.com>
2024-05-27 13:28:38 +0530

    backend url updated

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://vaymp1.vercel.app/api/v2";
 
-export const backend_url ="http://localhost:8000/";
+export const backend_url ="https://vaymp1.vercel.app/";
 
